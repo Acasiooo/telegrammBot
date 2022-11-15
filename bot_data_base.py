17628b16e1bcfe8b0e4b23cdf712cb75a7028679
Acasiooo <dimaniji@outlook.com>
2022-11-15 11:56:13 +0700

    testing data base complite

--- bot_data_base.py
+++ bot_data_base.py
@@ -3,13 +3,19 @@ import sqlite3
 
 def create_user_table(connect):
     cursor = connect.cursor()
-    cursor.execute('''CREATE TABLE users
-                      (id INTEGER PRIMARY KEY, user_id INTEGER UNIQUE NOT NULL, user_name TEXT NOT NULL,)''')
+    cursor.execute('''CREATE TABLE users (id INTEGER PRIMARY KEY, user_id INTEGER UNIQUE NOT NULL, user_name TEXT NOT NULL,)''')
     connect.commit()
     connect.close()
 
 
+def select_user(connect, user_id: int):
+    cursor = connect.cursor()
+    cursor.execute(f'''SELECT user_id FROM users WHERE user_id="{user_id}"''')
+    what_user = str(connect.fetchone())
+    connect.close()
+    return what_user == user_id
+
+
 def add_user_data(user_id: int, user_name: str, connect):
     cursor = connect.cursor()
-    cursor.execute(f'''INSERT INTO users (user_id, user_name)
-                      VALUES ({user_id, user_name})''')
+    cursor.execute(f'''INSERT INTO users (user_id, user_name) VALUES ({user_id, user_name})''')
--- mainProcess.py
+++ mainProcess.py
@@ -3,22 +3,37 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
+import bot_data_base
 from config import TOKEN
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
+connect = sqlite3.connect("bot_user_DB.db")
+
+
 @dp.message_handler(commands=["start"])
 async def process_start_command(message: types.Message):
     await message.reply("это стартовое сообщение")
+    await bot_data_base.create_user_table(connect)
 
 @dp.message_handler(commands=["help"])
 async def process_help_command(message: types.Message):
     await message.reply("это комманда help")
+    await bot_data_base.add_user_data(message.from_user.id, message.from_user.first_name, connect)
 
 @dp.message_handler()
 async def echo_message(message: types.Message):
-    await bot.send_message(message.from_user.id, message.text)
+    await bot.send_message(message.from_user.id, str(message.from_user.first_name))
+
+
+@dp.message_handler(commands=["test"])
+async def select_user(message: types.Message):
+    if bot_data_base.select_user():
+        await bot.send_message(message.from_user.id, str(message.from_user.first_name))
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
